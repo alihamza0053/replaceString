ed033602e932bcb075343602c7afb389317fe73a
malaika18khan <malaika18khan@gmail.com>
2023-03-27 16:48:33 +0500

    replace function with if condition

--- src/main/kotlin/replaceString.kt
+++ src/main/kotlin/replaceString.kt
@@ -1,15 +1,17 @@
-fun main(){
-    var string = "api.bytewise.com/{path}/{student}/kotlin/{name}/{rollnumber}"
-    println("running")
-    var a = string.repleaseKeys("path" to "android","student" to "CS","name" to "hamza", "rollnumber" to "50")
-    println(a)
 
+fun main() {
+    val string = "api.bytewise.com/{path}/{student}/kotlin/{name}/{rollnumber}"
+    val result = string.replaceKeys("path" to "android", "student" to "yes", "name" to "kashif", "rollnumber" to 3314)
+    println(result)
 }
 
-fun String.repleaseKeys(vararg values: Pair<Any,String>): String{
+fun String.replaceKeys(vararg values: Pair<String, Any>): String {
     var result = this
-    for ((key,value) in values){
-        result = result.replace("{$key}",value.toString())
+    for ((key, value) in values) {
+        val checkKey = "{$key}"
+        if (result.contains(checkKey)) {
+            result = result.replace(checkKey, value.toString())
+        }
     }
     return result
 }
